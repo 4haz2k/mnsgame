ccdc2549856c8b0656f462000bd605078a33f5c6
AS2005071645\Пользователь <zytia@mail.ru>
2022-06-30 11:54:54 +0300

    bug fix in settings page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\UserEditRequest;
 use App\Http\Services\ImageService;
+use App\Models\Game;
 use App\Models\User;
 use Illuminate\Contracts\Support\Renderable;
 use Illuminate\Http\RedirectResponse;
@@ -42,22 +43,29 @@ class HomeController extends Controller
     {
         $user = User::where("id", Auth::id())->first();
 
-        if($request->has("name"))
-            $user->name = $request->name;
+        if($request->has("name") and $request->filled('name'))
+            if($request->name != $user->name)
+                $user->name = $request->name;
 
-        if($request->has("surname"))
-            $user->surname = $request->surname;
+        if($request->has("surname") and $request->filled('surname'))
+            if($request->surname != $user->surname)
+                $user->surname = $request->surname;
 
-        if($request->has("login"))
-            $user->login = $request->login;
+        if($request->has("login") and $request->filled('login'))
+            if($request->login != $user->login)
+                $user->login = $request->login;
 
-        if($request->has("password"))
+        if($request->has("password") and $request->filled('password'))
             $user->password = Hash::make($request->password);
 
-        $user->email = $request->email;
+        if($request->has("email") and $request->filled('email'))
+            if($request->email != $user->email)
+                $user->email = $request->email;
 
-        $image = new ImageService();
-        $user->profile_image = $image->handleProfileUploadedImage($request);
+        if($request->hasFile("profile_image") and $request->filled('profile_image')){
+            $image = new ImageService();
+            $user->profile_image = $image->handleProfileUploadedImage($request);
+        }
 
         $user->save();
 
