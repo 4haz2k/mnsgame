075f88ca70fa7cbf7692e5af9ce588a8282013d0
AS2005071645\Пользователь <zytia@mail.ru>
2022-07-24 14:50:11 +0300

    updated config

--- config/telegram.php
+++ config/telegram.php
@@ -33,7 +33,7 @@ return [
     */
     'bots'                         => [
         'mnsgame' => [
-            'username'            => 'MNS Game Support',
+            'username'            => 'mnsgame_bot',
             'token'               => env('TELEGRAM_BOT_TOKEN', 'YOUR-BOT-TOKEN'),
             'certificate_path'    => env('TELEGRAM_CERTIFICATE_PATH', 'YOUR-CERTIFICATE-PATH'),
             'webhook_url'         => env('TELEGRAM_WEBHOOK_URL', 'YOUR-BOT-WEBHOOK-URL'),
