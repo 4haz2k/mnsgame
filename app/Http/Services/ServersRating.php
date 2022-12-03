cd678652f7d241b285180e182a4d04da173be9de
Alex <zytia@mail.ru>
2022-12-03 19:50:31 +0300

    fixed bug of payment

--- app/Http/Services/ServersRating.php
+++ app/Http/Services/ServersRating.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 
+use App\Http\Interfaces\PaymentTypes;
 use App\Models\PaymentHistory;
 use App\Models\Server;
 use App\Models\ServerRating;
@@ -23,17 +24,30 @@ class ServersRating
                 $server_rating = ServerRating::where("server_id", $server_payment->server_id)->first();
 
                 if($server_rating != null){
-                    $server_rating->rating -= $server_payment->balance_change;
+                    switch ($server_payment->type) {
+                        case PaymentTypes::packets[0]:
+                            $server_rating->rating -= $server_payment->balance_change;
+                            break;
+                        case PaymentTypes::packets[1]:
+                            $server_rating->rating -= 150;
+                            break;
+                        case PaymentTypes::packets[2]:
+                            $server_rating->rating -= 200;
+                            break;
+                        case PaymentTypes::packets[3]:
+                            $server_rating->rating -= 300;
+                            break;
+                    }
                     $server_rating->save();
                     $server_payment->is_active = true;
                     $server_payment->save();
                 }
 
-                if($server_payment->type != "rating"){
+                if($server_payment->type != PaymentTypes::packets[0]){
                     $server = Server::where("server_id", $server_payment->server_id)->first();
                     $server->background = null;
 
-                    if($server_payment->type == "packet3"){
+                    if($server_payment->type == PaymentTypes::packets[3]){
                         $server->chart = false;
                     }
 
