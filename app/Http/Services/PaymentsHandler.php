a327c75a2887ae07bf9dd6864962f4f7010a82ef
Aleksei <zytia@mail.com>
2022-12-14 17:25:37 +0300

    removed hard codes from payment logic

--- app/Http/Services/PaymentsHandler.php
+++ app/Http/Services/PaymentsHandler.php
@@ -4,73 +4,56 @@
 namespace App\Http\Services;
 
 
+use App\Http\Interfaces\PaymentTypes;
+
 class PaymentsHandler
 {
-    private array $packets = [
-        "rating" => 1,
-        "packet1" => 99,
-        "packet2" => 189,
-        "packet3" => 249,
-    ];
-
-    private array $selectedPaket;
+    private array $selectedPacket;
 
     public function __construct(string $type)
     {
         switch ($type){
-            case "rating":
-                $this->selectedPaket = [
-                    "packet" => "rating",
-                    "price" => $this->packets["rating"]
-                ];
+            case PaymentTypes::PACKET_RATING["title"]:
+                $this->selectedPacket = PaymentTypes::PACKET_RATING;
                 break;
-            case "packet1":
-                $this->selectedPaket = [
-                    "packet" => "packet1",
-                    "price" => $this->packets["packet1"]
-                ];
+            case PaymentTypes::PACKET_RUBY["title"]:
+                $this->selectedPacket = PaymentTypes::PACKET_RUBY;
                 break;
-            case "packet2":
-                $this->selectedPaket = [
-                    "packet" => "packet2",
-                    "price" => $this->packets["packet2"]
-                ];
+            case PaymentTypes::PACKET_SAPPHIRE["title"]:
+                $this->selectedPacket = PaymentTypes::PACKET_SAPPHIRE;
                 break;
-            case "packet3":
-                $this->selectedPaket = [
-                    "packet" => "packet3",
-                    "price" => $this->packets["packet3"]
-                ];
+            case PaymentTypes::PACKET_EMERALD["title"]:
+                $this->selectedPacket = PaymentTypes::PACKET_EMERALD;
                 break;
         }
     }
 
     public function getPacket(): array {
-        return $this->selectedPaket;
+        return $this->selectedPacket;
     }
 
     public function getRatingToSet(): int {
-        switch ($this->selectedPaket["packet"]) {
-            case "rating":
-                return 1;
-            case "packet1":
-                return 150;
-            case "packet2":
-                return 200;
-            case "packet3":
-                return 300;
+        switch ($this->selectedPacket) {
+            case PaymentTypes::PACKET_RATING:
+                return PaymentTypes::PACKET_RATING["rating"];
+            case PaymentTypes::PACKET_RUBY:
+                return PaymentTypes::PACKET_RUBY["rating"];
+            case PaymentTypes::PACKET_SAPPHIRE:
+                return PaymentTypes::PACKET_SAPPHIRE["rating"];
+            case PaymentTypes::PACKET_EMERALD:
+                return PaymentTypes::PACKET_EMERALD["rating"];
             default:
                 return 0;
         }
     }
 
     public function getTimeToSet(): string {
-        switch ($this->selectedPaket["packet"]) {
-            case "packet2":
-            case "packet3":
-            case "rating":
+        switch ($this->selectedPacket) {
+            case PaymentTypes::PACKET_SAPPHIRE:
+            case PaymentTypes::PACKET_EMERALD:
+            case PaymentTypes::PACKET_RATING:
                 return "1 month";
-            case "packet1":
+            case PaymentTypes::PACKET_RUBY:
                 return "1 week";
             default:
                 return 0;
