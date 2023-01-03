47bb22670b60317ca4dbc66fabd252895dc65003
Alex <zytia@mail.ru>
2023-01-03 21:34:24 +0300

    added favicon in svg format

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield("title")</title>
-    <link rel="icon" href="{{ asset("favicon.ico") }}" type="image/x-icon">
+    <link rel="icon" href="{{ asset("favicon.svg") }}" type="image/svg+xml">
     {!! SEOMeta::generate() !!}
     {!! OpenGraph::generate() !!}
     <script type="text/javascript" >
