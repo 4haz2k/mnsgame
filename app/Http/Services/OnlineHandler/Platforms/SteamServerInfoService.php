a8e303966110a8650caa17b94407f96302171c67
4haz2k <zytia@mail.ru>
2023-01-30 16:41:01 +0300

    Moved steam api request to guzzle query from curl

--- app/Http/Services/OnlineHandler/Platforms/SteamServerInfoService.php
+++ app/Http/Services/OnlineHandler/Platforms/SteamServerInfoService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services\OnlineHandler\Platforms;
 
 use App\Http\Services\OnlineHandler\ServerInfo;
+use GuzzleHttp\Exception\GuzzleException;
 
 class SteamServerInfoService extends ServerInfo
 {
@@ -16,30 +17,21 @@ class SteamServerInfoService extends ServerInfo
      * Making query on Steam API
      *
      * @return bool
+     * @throws GuzzleException
      */
     protected function makeQuery(): bool
     {
         $server_address = $this->serverPort == null ? $this->serverIp : $this->serverIp.":".$this->serverPort;
 
-        $url = "https://api.steampowered.com/IGameServersService/GetServerList/v1/?filter=\appid\\{$this->serverAppId}\addr\\{$server_address}&key=".config("steam.STEAM_API_KEY");
+        $data = $this->getApiData(
+            "https://api.steampowered.com/IGameServersService/GetServerList/v1/?filter=\appid\\{$this->serverAppId}\addr\\{$server_address}&key=" . config("steam.STEAM_API_KEY")
+        );
 
-        $ch = curl_init();
-
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-        curl_setopt($ch, CURLOPT_HEADER, 0);
-
-        $result = curl_exec($ch);
-
-        curl_close($ch);
-
-        $result = json_decode($result, true);
-
-        if(empty($result["response"])){
+        if(empty((array)$data->response)){
            return false;
         }
 
-        $this->playersCount = $result["response"]["servers"][0]["players"];
+        $this->playersCount = $data->response->servers[0]->players;
 
         return true;
     }
@@ -49,6 +41,7 @@ class SteamServerInfoService extends ServerInfo
      * Getting players count
      *
      * @return int
+     * @throws GuzzleException
      */
     public function getPlayersCount(): int
     {
